08534fa92a4d4705a373f2feec5fd684bcbf3f69
Unlimited Dreams <UnlimitedDreamsTI@gmail.com>
2015-05-08 10:11:42 -0500

    Realease 3.1

--- UFilesII/build/generated/src/org/apache/jsp/vista/main_jsp.java
+++ UFilesII/build/generated/src/org/apache/jsp/vista/main_jsp.java
@@ -50,22 +50,24 @@ public final class main_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("\n");
       out.write("    <h:head>\n");
       out.write("        <h:meta charset=\"utf-8\"></h:meta>\n");
-      out.write("        <h:title>Unlimited Files</h:title>\n");
+      out.write("        <title>Unlimited Files</title>\n");
       out.write("        <h:meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n");
+      out.write("        <link rel=\"icon\" href=\"../img/unlimtd.ico\"/>\n");
+      out.write("\n");
       out.write("        <!-- css -->\n");
       out.write("        <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" />\n");
-      out.write("        <link  href = \"bootstrap-3.3.4/css/bootstrap.css\"  rel = \"stylesheet\" ></link>\n");
-      out.write("        <script src=\"Jquery/jquery-1.11.2.js\"></script>\n");
+      out.write("        <link  href = \"../bootstrap-3.3.4/css/bootstrap.css\"  rel = \"stylesheet\" ></link>\n");
+      out.write("        <script src=\"../Jquery/jquery-1.11.2.js\"></script>\n");
       out.write("    </h:head>\n");
       out.write("    <h:body>\n");
       out.write("        <nav  class = \"navbar navbar-inverse navbar-fixed-top\" > \n");
       out.write("            <div  class= \"container-fluid\" >\n");
       out.write("                <div  class= \"navbar-header\" > \n");
       out.write("                    <a  class= \"navbar-brand\"  href= \"#\" > \n");
-      out.write("                        <img src= \"img/text3338.png\" style=\"width: 27px; height: 27px;\"></img> \n");
+      out.write("                        <img src= \"../img/text3338.png\" style=\"width: 27px; height: 27px;\"></img> \n");
       out.write("                    </a> \n");
       out.write("                    <ul class=\"nav navbar-nav\"> \n");
-      out.write("                        <li> <a  href=\"inicio.html\"><h4 style=\"color:#006400\">Home</h4></a></li>\n");
+      out.write("                        <li> <a  href=\"../inicio.html\"><h4 style=\"color:#006400\">Home</h4></a></li>\n");
       out.write("                        <li><a href=\"http://getbootstrap.com/examples/starter-template/#\"><h4 style=\"color:#006400\">Productos</h4></a></li>\n");
       out.write("                        <li><a href=\"http://getbootstrap.com/examples/starter-template/#\"><h4 style=\"color:#006400\">Contactos</h4></a></li>\n");
       out.write("                    </ul>                 \n");
@@ -86,38 +88,48 @@ public final class main_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("        <br></br>\n");
       out.write("\n");
       out.write("        <div class=\"container\">\n");
-      out.write("            <div class=\"row\">\n");
-      out.write("                <h1>GESTION DE ADMINISTRADOR</h1>\n");
-      out.write("                <!--<div class=\"col-xs-6 col-md-4\">\n");
-      out.write("                    <a href=\"gestionUsuariosAdmon.html\" class=\"thumbnail\" >\n");
-      out.write("                        <img src=\"img/slides/user197.png\" style=\"height: 100px; width: 100px\" />\n");
-      out.write("                    </a>\n");
-      out.write("                    <div class=\"caption\">\n");
-      out.write("                        <center> <h3>Gestion de usuarios</h3></center>\n");
-      out.write("                    </div>\n");
-      out.write("                </div>-->\n");
+      out.write("            ");
+
+                int rol = Integer.parseInt(session.getAttribute("idRol").toString());
+                if (rol == 1) {
+                    out.println("<div class=\"row\"><h1>GESTION DE ADMINISTRADOR</h1><div class=\"col - xs - 6 col - md - 4\">"
+                            + "<a href = \"../Crud_roles.html\" class=\"thumbnail\">"
+                            + "<img src=\"../img/slides/network11.png\" style=\"height: 100px; width: 100px\" />"
+                            + "</a><div class=\"caption\"><center> <h3>Gestion de  roles</h3></center>"
+                            + "</div></div><div class=\"col-xs-6 col-md-4\">"
+                            + "<a href=\"../crud_Grupos.jsp\" class=\"thumbnail\">"
+                            + "<img src=\"../img/slides/group9.png\" style=\"height: 100px ; width: 100px\"/>"
+                            + "</a><div class=\"caption\"><center><h3>Gestion de  grupos de trabajo</h3></center>"
+                            + "</div></div><div class=\"col-xs-6 col-md-4\">"
+                            + "<a href=\"../Crud_usuario_admon.jsp\" class=\"thumbnail\">"
+                            + "<img src=\"../img/slides/group9.png\" style=\"height: 100px ; width: 100px\"/>"
+                            + "</a><div class=\"caption\"><center><h3>Gestion de  Usuarios</h3></center>"
+                            + "</div></div></div>");
+                } 
+
+
+            
       out.write("\n");
-      out.write("                <div class=\"col-xs-6 col-md-4\">\n");
-      out.write("                    <a href=\"Crud_roles.html\" class=\"thumbnail\">\n");
-      out.write("                        <img src=\"img/slides/network11.png\" style=\"height: 100px; width: 100px\" />\n");
+      out.write("\n");
+      out.write("            <div class=\"row\"><h1>GESTION DE ADMINISTRADOR</h1><div class=\"col-xs-6 col-md-4\">\n");
+      out.write("                    <a href=\"../Crud_roles.html\" class=\"thumbnail\">\n");
+      out.write("                        <img src=\"../img/slides/network11.png\" style=\"height: 100px; width: 100px\" />\n");
       out.write("                    </a>\n");
       out.write("                    <div class=\"caption\">\n");
       out.write("                        <center>  <h3>Gestion de  roles</h3></center>\n");
-      out.write("\n");
       out.write("                    </div>\n");
       out.write("                </div>\n");
-      out.write("\n");
       out.write("                <div class=\"col-xs-6 col-md-4\">\n");
-      out.write("                    <a href=\"crud_Grupos.jsp\" class=\"thumbnail\">\n");
-      out.write("                        <img src=\"img/slides/group9.png\" style=\"height: 100px ; width: 100px\" />\n");
+      out.write("                    <a href=\"../crud_Grupos.jsp\" class=\"thumbnail\">\n");
+      out.write("                        <img src=\"../img/slides/group9.png\" style=\"height: 100px ; width: 100px\" />\n");
       out.write("                    </a>\n");
       out.write("                    <div class=\"caption\">\n");
       out.write("                        <center>  <h3>Gestion de  grupos de trabajo</h3></center>\n");
       out.write("                    </div>                   \n");
       out.write("                </div>\n");
       out.write("                <div class=\"col-xs-6 col-md-4\">\n");
-      out.write("                    <a href=\"Crud_usuario_admon.jsp\" class=\"thumbnail\">\n");
-      out.write("                        <img src=\"img/slides/group9.png\" style=\"height: 100px ; width: 100px\" />\n");
+      out.write("                    <a href=\"../Crud_usuario_admon.jsp\" class=\"thumbnail\">\n");
+      out.write("                        <img src=\"../img/slides/group9.png\" style=\"height: 100px ; width: 100px\" />\n");
       out.write("                    </a>\n");
       out.write("                    <div class=\"caption\">\n");
       out.write("                        <center>  <h3>Gestion de  Usuarios</h3></center>\n");
@@ -129,17 +141,17 @@ public final class main_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("                <br></br>\n");
       out.write("                <h1>GESTION OPERARIA</h1>\n");
       out.write("                <div class=\"col-xs-6 col-md-4\">\n");
-      out.write("                    <a href=\"Subir_Documento_2.html\" class=\"thumbnail\" >\n");
-      out.write("                        <img src=\"img/slides/suitcase6.png\" style=\"height: 100px ; width: 100px\" />\n");
+      out.write("                    <a href=\"../Subir_Documento_2.html\" class=\"thumbnail\" >\n");
+      out.write("                        <img src=\"../img/slides/suitcase6.png\" style=\"height: 100px ; width: 100px\" />\n");
       out.write("                    </a>\n");
       out.write("                    <div class=\"caption\">\n");
       out.write("                        <center> <h3>Subir archivos</h3></center>\n");
       out.write("                    </div>\n");
       out.write("                </div>\n");
-      out.write("              \n");
+      out.write("\n");
       out.write("                <div class=\"col-xs-6 col-md-4\">\n");
-      out.write("                    <a href=\"MisGrupos.html\" class=\"thumbnail\">\n");
-      out.write("                        <img src=\"img/slides/manager3 .png\" style=\"height: 100px ; width: 100px\" />\n");
+      out.write("                    <a href=\"../MisGrupos.html\" class=\"thumbnail\">\n");
+      out.write("                        <img src=\"../img/slides/manager3 .png\" style=\"height: 100px ; width: 100px\" />\n");
       out.write("                    </a>\n");
       out.write("                    <div class=\"caption\">\n");
       out.write("                        <center>  <h3>Mis grupos</h3></center>\n");
@@ -147,8 +159,8 @@ public final class main_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("                    </div>\n");
       out.write("                </div>\n");
       out.write("                <div class=\"col-xs-6 col-md-4\">\n");
-      out.write("                    <a href=\"MisDatos.jsp\" class=\"thumbnail\">\n");
-      out.write("                        <img src=\"img/slides/manager3 .png\" style=\"height: 100px ; width: 100px\" />\n");
+      out.write("                    <a href=\"../MisDatos.jsp\" class=\"thumbnail\">\n");
+      out.write("                        <img src=\"../img/slides/manager3 .png\" style=\"height: 100px ; width: 100px\" />\n");
       out.write("                    </a>\n");
       out.write("                    <div class=\"caption\">\n");
       out.write("                        <center>  <h3>Mis Datos</h3></center>\n");
@@ -160,7 +172,7 @@ public final class main_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("\n");
       out.write("        <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script>\n");
       out.write("        <script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js\"></script>\n");
-      out.write("        <script src = \"bootstrap-3.3.4/js/bootstrap.min.js\" ></script>\n");
+      out.write("        <script src = \"../bootstrap-3.3.4/js/bootstrap.min.js\" ></script>\n");
       out.write("    </h:body>\n");
       out.write("</xhtml>\n");
       out.write("\n");
